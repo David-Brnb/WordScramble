74fa7f692d71e09c13acd09dc240db02d02a570e
David-Brnb <321171197@ciencias.unam.mx>
2024-12-22 22:43:45 -0600

    add: some stuff of second class

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,14 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var vari:String = ""
+    let people = ["Juan", "Ari", "Pedro", "Pepe", "Chalino"]
+    @State private var isEnabled = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Image(systemName: "globe")
+            .imageScale(.large)
+            .foregroundStyle(.tint)
+        Text("Hello World")
+            .padding()
+        Button("ok"){
+            testString()
         }
-        .padding()
+    }
+    
+    func testString() {
+        let word = "swift"
+        let checker = UITextChecker()
+        
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        let allGood = misspelledRange.location == NSNotFound
     }
 }
 
